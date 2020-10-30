7c767d9bd3e6ad1aa62ac02c2eb882f3c4d92c57
sanaxaasif <73714561+sanaxaasif@users.noreply.github.com>
2020-10-31 00:35:51 +0500

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -34,8 +34,8 @@ int binarySearch(int array[], int numElems, int value)	// function heading
 {
 	int first = 0;				// First element of list
 	int last = numElems - 1;	// last element of the list
-	int middle;					// variable containing the current
-								// middle value of the list
+	int middle;					// variable containing the current middle value of the list
+								
 
 	while (first <= last)
 	{
